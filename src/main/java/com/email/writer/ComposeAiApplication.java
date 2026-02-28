3cab9dda7840ede0464bf74542b05f0dab4fffd8
Tejas Bansal <tejasbansal6065@gmail.com>
2026-03-01 00:34:39 +0530

    smart email composer

---
+++ src/main/java/com/email/writer/ComposeAiApplication.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComposeAiApplication.class, args);
+	}
+
+}
